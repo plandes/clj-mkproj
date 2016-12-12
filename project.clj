759b0206dcabe7e667b02dc9aec758635c7588be
Paul Landes <landes@mailc.net>
2016-12-12 13:31:53 -0600

    dep version

--- project.clj
+++ project.clj
@@ -31,7 +31,7 @@
                  [clj-yaml "0.4.0"]
 
                  ;; command line
-                 [com.zensols.tools/actioncli "0.0.11"]]
+                 [com.zensols.tools/actioncli "0.0.12"]]
   :pom-plugins [[org.codehaus.mojo/appassembler-maven-plugin "1.6"
                  {:configuration ([:programs
                                    [:program
