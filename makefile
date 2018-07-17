6305561c11b1ff2e671a3fecca742157290ba261
Paul Landes <landes@mailc.net>
2018-07-17 15:34:56 -0500

    build; license

--- makefile
+++ makefile
@@ -2,9 +2,13 @@
 
 # type of project, currently one of: clojure, python
 PROJ_TYPE=		clojure
-PROJ_MODULES=		dist release
+PROJ_MODULES=		appassem release
 
-include $(if $(ZBHOME),$(ZBHOME),../zenbuild)/main.mk
+# make build dependencies
+_ :=	$(shell [ ! -d .git ] && git init ; [ ! -d zenbuild ] && \
+	  git submodule add https://github.com/plandes/zenbuild && make gitinit )
+
+include ./zenbuild/main.mk
 
 .PHONY:	test
 test:
--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
 (defproject com.zensols.tools/mkproj "0.1.0-SNAPSHOT"
   :description "Generates boilerplate projects"
   :url "https://github.com/plandes/Natural Language Parse"
-  :license {:name "Apache License version 2.0"
-            :url "https://www.apache.org/licenses/LICENSE-2.0"
+  :license {:name "MIT"
+            :url "https://opensource.org/licenses/MIT"
             :distribution :repo}
-  :plugins [[lein-codox "0.10.1"]
+  :plugins [[lein-codox "0.10.3"]
             [org.clojars.cvillecsteele/lein-git-version "1.2.7"]]
   :codox {:metadata {:doc/format :markdown}
           :project {:name "Boilerplate projects"}
@@ -42,11 +42,11 @@
                                     ([:mainClass "zensols.mkproj.core"]
                                      [:id "mkproj"])]]
                                   [:environmentSetupFileName "setupenv"])}]]
-  :profiles {:clojure-1.8 {:dependencies [[org.clojure/clojure "1.8.0"]]}
+  :profiles {:1.9 {:dependencies [[org.clojure/clojure "1.9.0"]]}
              :uberjar {:aot [zensols.mkproj.core]}
              :appassem {:aot :all}
+             :snapshot {:git-version {:version-cmd "echo -snapshot"}}
              :test
-             {:jvm-opts
-              ["-Dlog4j.configurationFile=test-resources/test-log4j2.xml"
-               "-Xms4g" "-Xmx12g" "-XX:+UseConcMarkSweepGC"]}}
+             {:jvm-opts ["-Dlog4j.configurationFile=test-resources/test-log4j2.xml"
+                         "-Xmx12g"]}}
   :main zensols.mkproj.core)
